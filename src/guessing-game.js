f50b084203a413b509b91c74c2d4a529ec2fef02
Looofeee <Looofeee@mail.ru>
2017-09-24 18:41:06 +0300

    cearly all

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,24 @@
 class GuessingGame {
-    constructor() {}
+
+    constructor() {};
 
     setRange(min, max) {
+        this.min = min;
+        this.max = max;
+    };
 
-    }
-
-    guess() {
-
-    }
+    guess(){   
+        return Math.ceil((this.max+this.min+1)/2);
+    };
 
     lower() {
-
-    }
+        this.setRange(this.min,Math.floor((this.max+this.min)/2));
+    };
 
     greater() {
+        this.setRange(Math.floor((this.max+this.min)/2+1),this.max);
+    };
 
-    }
 }
 
 module.exports = GuessingGame;
